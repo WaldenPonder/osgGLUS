74257575c2910d1b4e3de2686f1129f9cfaea761
wangstu <wangqianstu@163.com>
2019-10-15 15:13:45 +0800

    -

--- out_line/outline.cpp
+++ out_line/outline.cpp
@@ -10,9 +10,12 @@
 #include <osgViewer/Viewer>
 #include <sstream>
 
-osg::Camera* hudCamera;
+osg::Camera*   hudCamera  = new osg::Camera;
+osg::Group*	root		  = new osg::Group();
+osg::Camera*   fbo_camera = new osg::Camera;
+osg::Geometry* screenQuat = nullptr;
 
-void test1(osg::Group* root, osg::Node* node);
+void set_up(float w, float h, osgViewer::Viewer* view);
 
 struct MyCameraPostDrawCallback : public osg::Camera::DrawCallback
 {
@@ -92,11 +95,11 @@ class camere_callback : public osg::Callback
 	camere_callback(osg::Camera* c) : c_(c) {}
 	virtual bool run(osg::Object* object, osg::Object* data)
 	{
-		// osg::Camera* camera = dynamic_cast<osg::Camera*>(object);
-		// string name = camera->getName();
+		osg::Camera* camera = dynamic_cast<osg::Camera*>(object);
+		string		 name   = camera->getName();
 
-		// camera->setProjectionMatrix(c_->getProjectionMatrix());
-		// camera->setViewMatrix(c_->getViewMatrix());
+		camera->setProjectionMatrix(c_->getProjectionMatrix());
+		camera->setViewMatrix(c_->getViewMatrix());
 		// hudCamera->setViewport(c_->getViewport());
 
 		return traverse(object, data);
@@ -108,21 +111,37 @@ class PickHandler : public osgGA::GUIEventHandler
 
  public:
 	osg::Camera* fbo_camera;
-	PickHandler(osg::Camera* c) : fbo_camera(c) {}
+	PickHandler(osg::Camera* fbo) : fbo_camera(fbo) {}
 
 	~PickHandler() {}
 
 	bool handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa)
 	{
+		osgViewer::Viewer* view = dynamic_cast<osgViewer::Viewer*>(&aa);
+		fbo_camera->setProjectionMatrix(view->getCamera()->getProjectionMatrix());
+		fbo_camera->setViewMatrix(view->getCamera()->getViewMatrix());
+
 		switch (ea.getEventType())
 		{
 		case (osgGA::GUIEventAdapter::FRAME):
 		{
 
-			osgViewer::View* view = dynamic_cast<osgViewer::View*>(&aa);
-			fbo_camera->setProjectionMatrix(view->getCamera()->getProjectionMatrix());
-			fbo_camera->setViewMatrix(view->getCamera()->getViewMatrix());
-			// fbo_camera->setViewport(view->getCamera()->getViewport());
+			//if (screenQuat)
+			//{
+			//	screenQuat->dirtyGLObjects();
+			//}
+
+			//fbo_camera->setViewport(view->getCamera()->getViewport());
+			break;
+		}
+		case (osgGA::GUIEventAdapter::KEYDOWN):
+		{
+			if (ea.getKey() == osgGA::GUIEventAdapter::KEY_A)
+			{
+				osg::Viewport* vp = view->getCamera()->getViewport();
+				set_up(vp->width(), vp->height(), view);
+			}
+			break;
 		}
 		}
 		return false;
@@ -131,12 +150,8 @@ class PickHandler : public osgGA::GUIEventHandler
 	virtual void pick(osgViewer::View* view, const osgGA::GUIEventAdapter& ea) {}
 };
 
-int main(int argc, char** argv)
+void set_up(float w, float h, osgViewer::Viewer* view)
 {
-	osg::ArgumentParser arguments(&argc, argv);
-
-	osg::setNotifyLevel(osg::NotifySeverity::INFO);
-
 	std::vector<osg::Vec3d> allPTs;
 	allPTs.push_back(osg::Vec3(0, 0, 0));
 	allPTs.push_back(osg::Vec3(100, 0, 0));
@@ -144,215 +159,130 @@ int main(int argc, char** argv)
 
 	osg::DisplaySettings::instance()->setNumMultiSamples(4);
 
-	osg::Node* node =
-		createLine(allPTs, osg::Vec4(0, 1, 1, 1), osg::PrimitiveSet::LINE_STRIP);
-	// osg::Node* node = osgDB::readNodeFile("cow.osg");
+	//osg::Node* node =
+	//	createLine(allPTs, osg::Vec4(0, 1, 1, 1), osg::PrimitiveSet::LINE_STRIP);
+	//osg::Node* node = osgDB::readNodeFile("D:\\Shader\\cube.obj"); //osgDB::readNodeFile("cow.osg");
+	osg::Node* node = osgDB::readNodeFile("E:\\FileRecv\\abc.osg");
 
-	osg::Group* root = new osg::Group();
-	// root->addChild(node);
+	//root->addChild(node);
 	osg::PositionAttitudeTransform* pat = new osg::PositionAttitudeTransform;
-	pat->setPosition(osg::Vec3(0, 0, 10));
+	//pat->setPosition(osg::Vec3(0, 15, 0));
 	pat->addChild(node);
 	root->addChild(pat);
 
-	osgViewer::Viewer viewer(arguments);
-	viewer.setSceneData(root);
-	viewer.setUpViewAcrossAllScreens();
+	//viewer.setUpViewAcrossAllScreens();
 
 	osg::Texture2D* texture2D = new osg::Texture2D;
-	osg::Geometry*  gem		  = osg::createTexturedQuadGeometry(
-		   osg::Vec3(), osg::Vec3(1280, 0, 0), osg::Vec3(0, 1024, 0));
+	screenQuat				  = osg::createTexturedQuadGeometry(
+		   osg::Vec3(), osg::Vec3(w, 0, 0), osg::Vec3(0, h, 0));
 	{
-		// osgViewer::Viewer::Windows windows;
-		// viewer.getWindows(windows);
-
-		hudCamera = new osg::Camera;
 		hudCamera->setReferenceFrame(osg::Transform::ABSOLUTE_RF);
-		hudCamera->setProjectionMatrixAsOrtho2D(0, 1280, 0, 1024);
+		hudCamera->setProjectionMatrixAsOrtho2D(0, w, 0, h);
 		hudCamera->setViewMatrix(osg::Matrix::identity());
 		hudCamera->setRenderOrder(osg::Camera::POST_RENDER);
 		hudCamera->setClearMask(GL_DEPTH_BUFFER_BIT);
 		hudCamera->getOrCreateStateSet()->setMode(GL_LIGHTING,
 												  osg::StateAttribute::OFF);
 		root->addChild(hudCamera);
-		// set up cameras to render on the first window available.
-		// hudCamera->setGraphicsContext(windows[0]);
-		// hudCamera->setViewport(0, 0, windows[0]->getTraits()->width,
-		// windows[0]->getTraits()->height); root->addChild(hudCamera);
 
 		osg::Geode* geode = new osg::Geode;
 		hudCamera->addChild(geode);
-		geode->addChild(gem);
+		geode->addChild(screenQuat);
 		geode->getOrCreateStateSet()->setRenderingHint(
 			osg::StateSet::TRANSPARENT_BIN);
 		geode->getOrCreateStateSet()->setMode(GL_BLEND, osg::StateAttribute::ON);
 
-		gem->setDataVariance(osg::Object::DYNAMIC);
-		gem->setSupportsDisplayList(false);
+		screenQuat->setDataVariance(osg::Object::DYNAMIC);
+		screenQuat->setSupportsDisplayList(false);
 
 		texture2D->setTextureSize(1024, 1024);
 		texture2D->setInternalFormat(GL_RGBA);
 		texture2D->setFilter(osg::Texture2D::MIN_FILTER, osg::Texture2D::LINEAR);
 		texture2D->setFilter(osg::Texture2D::MAG_FILTER, osg::Texture2D::LINEAR);
 
-		osg::StateSet* stateset = geode->getOrCreateStateSet();
+		osg::StateSet* ss = geode->getOrCreateStateSet();
 
-		stateset->setTextureAttributeAndModes(0, texture2D,
-											  osg::StateAttribute::ON);
+		ss->setTextureAttributeAndModes(0, texture2D,
+										osg::StateAttribute::ON);
+
+		osg::ref_ptr<osg::Program> program = new osg::Program;
+		osg::Shader*			   vert	= osgDB::readShaderFile(osg::Shader::VERTEX, "outline.vert");
+		osg::Shader*			   frag	= osgDB::readShaderFile(osg::Shader::FRAGMENT, "outline.frag");
+		program->addShader(vert);
+		program->addShader(frag);
+		ss->addUniform(new osg::Uniform("baseTexture", 0));
+		ss->addUniform(new osg::Uniform("u_screen_width", w));
+		ss->addUniform(new osg::Uniform("u_screen_height", h));
+		ss->setAttributeAndModes(program, osg::StateAttribute::ON);
 	}
+	root->addChild(fbo_camera);
 
 	// then create the camera node to do the render to texture
+
+	// add subgraph to render
+	fbo_camera->addChild(node);
+
+	//view->addSlave(fbo_camera, false);
+	fbo_camera->setName("FBO_CAMERA");
+	//fbo_camera->addEventCallback(new camere_callback(viewer.getCamera()));
+	// set up the background color and clear mask.
+	fbo_camera->setClearColor(osg::Vec4());
+	fbo_camera->setClearMask(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// set view
+	fbo_camera->setReferenceFrame(osg::Transform::ABSOLUTE_RF);
+	// set viewport
+	fbo_camera->setViewport(0, 0, 1024, 1024);
+
+	// set the camera to render before the main camera.
+	fbo_camera->setRenderOrder(osg::Camera::PRE_RENDER);
+
+	// tell the camera to use OpenGL frame buffer object where supported.
+	fbo_camera->setRenderTargetImplementation(osg::Camera::FRAME_BUFFER_OBJECT);
+
+	bool useImage = false;
+	if (useImage)
 	{
-		osg::Camera* camera = new osg::Camera;
-		camera->setName("FBO_CAMERA");
-		camera->addEventCallback(new camere_callback(viewer.getCamera()));
-		// set up the background color and clear mask.
-		camera->setClearColor(osg::Vec4());
-		camera->setClearMask(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		// set view
-		camera->setReferenceFrame(osg::Transform::ABSOLUTE_RF);
-		// set viewport
-		camera->setViewport(0, 0, 1024, 1024);
+		osg::Image* image = new osg::Image;
+		image->allocateImage(1024, 1024, 1, GL_RGBA, GL_FLOAT);
 
-		// set the camera to render before the main camera.
-		camera->setRenderOrder(osg::Camera::POST_RENDER);
+		// attach the image so its copied on each frame.
+		fbo_camera->attach(osg::Camera::COLOR_BUFFER, image, 4, 4);
+		// camera->setPostDrawCallback(new MyCameraPostDrawCallback(image));
 
-		// tell the camera to use OpenGL frame buffer object where supported.
-		camera->setRenderTargetImplementation(osg::Camera::FRAME_BUFFER_OBJECT);
-
-		if (1)
-		{
-			osg::Image* image = new osg::Image;
-			image->allocateImage(1024, 1024, 1, GL_RGBA, GL_FLOAT);
-
-			// attach the image so its copied on each frame.
-			camera->attach(osg::Camera::COLOR_BUFFER, image, 4, 4);
-			// camera->setPostDrawCallback(new MyCameraPostDrawCallback(image));
-
-			// Rather than attach the texture directly to illustrate the texture's
-			// ability to detect an image update and to subload the image onto the
-			// texture.  You needn't do this when using an Image for copying to, as a
-			// separate camera->attach(..) would suffice as well, but we'll do it the
-			// long way round here just for demonstration purposes (long way round
-			// meaning we'll need to copy image to main memory, then copy it back to
-			// the graphics card to the texture in one frame). The long way round
-			// allows us to manually modify the copied image via the callback and then
-			// let this modified image by reloaded back.
-			texture2D->setImage(0, image);
-		}
-		else
-		{
-			// attach the texture and use it as the color buffer.
-			camera->attach(osg::Camera::COLOR_BUFFER, texture2D, 0, 0, false);
-			// samples, colorSamples);
-		}
-
-		// add subgraph to render
-		camera->addChild(node);
-		root->addChild(camera);
-
-		viewer.addEventHandler(new PickHandler(camera));
-		// viewer.addSlave(camera, false);
+		// Rather than attach the texture directly to illustrate the texture's
+		// ability to detect an image update and to subload the image onto the
+		// texture.  You needn't do this when using an Image for copying to, as a
+		// separate camera->attach(..) would suffice as well, but we'll do it the
+		// long way round here just for demonstration purposes (long way round
+		// meaning we'll need to copy image to main memory, then copy it back to
+		// the graphics card to the texture in one frame). The long way round
+		// allows us to manually modify the copied image via the callback and then
+		// let this modified image by reloaded back.
+		texture2D->setImage(0, image);
 	}
+	else
+	{
+		// attach the texture and use it as the color buffer.
+		fbo_camera->attach(osg::Camera::COLOR_BUFFER, texture2D,
+						   0, 0, false,
+						   4, 4);
+	}
+
+	fbo_camera->setProjectionMatrix(view->getCamera()->getProjectionMatrix());
+	fbo_camera->setViewMatrix(view->getCamera()->getViewMatrix());
+}
+
+int main(int argc, char** argv)
+{
+	osg::ArgumentParser arguments(&argc, argv);
+
+	osg::setNotifyLevel(osg::NotifySeverity::INFO);
+
+	osgViewer::Viewer viewer(arguments);
+	viewer.setSceneData(root);
+	viewer.addEventHandler(new PickHandler(fbo_camera));
 
 	add_event_handler(viewer);
 
 	return viewer.run();
-}
-
-void test1(osg::Group* root, osg::Node* node)
-{
-	int N = 1;
-	for (int i = 0; i < N; i++)
-	{
-		float offset = 1e-3;
-
-		const char* VS =
-			"#version 330 compatibility \n"
-			"uniform float outlineWidth; \n"
-			"void main()"
-			"{ \n"
-			"    vec4 clip = gl_ModelViewProjectionMatrix * gl_Vertex; \n"
-			"    vec3 normal = normalize(gl_NormalMatrix * gl_Normal); \n"
-			"    normal.x *= gl_ProjectionMatrix[0][0]; \n"
-			"    normal.y *= gl_ProjectionMatrix[1][1]; \n"
-			"    clip.xy += normal.xy * clip.w * outlineWidth;\n"
-			"    gl_Position = clip; \n"
-			"} \n";
-
-		std::stringstream fs_;
-		fs_ << "#version 330\n"
-			   "void main() \n"
-			   "{ \n"
-			<< "float a = " << ((1.f / N) * (N - i)) << " ;\n"
-			<< " gl_FragColor = vec4(1,0,0, 1);\n"
-			<< "}\n";
-
-		string		ss = fs_.str();
-		const char* FS = ss.c_str();
-
-		osg::Program* program = new osg::Program();
-		program->addShader(new osg::Shader(osg::Shader::VERTEX, VS));
-		program->addShader(new osg::Shader(osg::Shader::FRAGMENT, FS));
-
-		osg::AutoTransform* at = new osg::AutoTransform;
-		at->setAutoRotateMode(osg::AutoTransform::NO_ROTATION);
-		root->addChild(at);
-
-		osg::Group* outline = new osg::Group();
-		{
-			osg::Program* program = new osg::Program();
-			program->addShader(new osg::Shader(osg::Shader::VERTEX, VS));
-			program->addShader(new osg::Shader(osg::Shader::FRAGMENT, FS));
-
-			osg::StateSet* outlineSS = outline->getOrCreateStateSet();
-			outlineSS->setRenderBinDetails(1, "RenderBin");
-			outlineSS->setAttributeAndModes(
-				new osg::Depth(osg::Depth::LESS, 0.0, 1.0, false));
-			outlineSS->setMode(GL_CULL_FACE, 0);
-			outlineSS->setAttribute(program);
-			outlineSS->addUniform(new osg::Uniform("outlineWidth", offset));
-			outlineSS->setAttributeAndModes(new osg::LineWidth(2),
-											osg::StateAttribute::ON);
-			outline->addChild(node);
-
-			outlineSS->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
-			outlineSS->setMode(GL_BLEND, osg::StateAttribute::ON);
-			// outlineSS->setRenderingHint(osg::StateSet::TRANSPARENT_BIN);
-			outlineSS->setAttributeAndModes(
-				new osg::BlendFunc(osg::BlendFunc::SRC_ALPHA,
-								   osg::BlendFunc::ONE_MINUS_SRC_ALPHA),
-				osg::StateAttribute::ON);
-		}
-
-		osg::Group* outline2 = new osg::Group();
-		{
-			osg::StateSet* outlineSS = outline2->getOrCreateStateSet();
-			outlineSS->setRenderBinDetails(1, "RenderBin");
-			outlineSS->setAttributeAndModes(
-				new osg::Depth(osg::Depth::LESS, 0.0, 1.0, false));
-			outlineSS->setMode(GL_CULL_FACE, 0);
-			outlineSS->setAttribute(program);
-			outlineSS->addUniform(new osg::Uniform("outlineWidth", -offset));
-			outlineSS->setAttributeAndModes(new osg::LineWidth(2),
-											osg::StateAttribute::ON);
-			outline2->addChild(node);
-
-			outlineSS->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
-			outlineSS->setMode(GL_BLEND, osg::StateAttribute::ON);
-			// outlineSS->setRenderingHint(osg::StateSet::TRANSPARENT_BIN);
-			outlineSS->setAttributeAndModes(
-				new osg::BlendFunc(osg::BlendFunc::SRC_ALPHA,
-								   osg::BlendFunc::ONE_MINUS_SRC_ALPHA),
-				osg::StateAttribute::ON);
-		}
-
-		at->addChild(outline);
-		at->addChild(outline2);
-	}
-
-	osg::Group* model = new osg::Group();
-	model->getOrCreateStateSet()->setRenderBinDetails(2, "RenderBin");
-	model->addChild(node);
-	root->addChild(model);
 }
---
+++ out_line/outline.frag
@@ -0,0 +1,65 @@
+uniform sampler2D baseTexture;
+
+varying vec3 vNormal; 
+varying vec3 vViewPosition; 
+varying vec4 vColor;
+varying vec2 texcoord;
+
+uniform float u_screen_width, u_screen_height;
+uniform float weight[5] = float[] (0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216);
+
+float rgb2gray(vec3 color) {
+    return 0.2126 * color.r + 0.7152 * color.g + 0.0722 * color.b;
+}
+
+// in vec2 texcoord;
+float pixel_operator(float dx, float dy) {
+    vec4 rgba_color = texture( baseTexture, texcoord + vec2(dx,dy) );
+    return rgb2gray(rgba_color.rgb);
+}
+
+float sobel_filter()
+{
+    float dx = 1.0 / float(u_screen_width); // e.g. 1920
+    float dy = 1.0 / float(u_screen_height); // e.g. 1080
+
+    float s00 = pixel_operator(-dx, dy);
+    float s10 = pixel_operator(-dx, 0.0);
+    float s20 = pixel_operator(-dx, -dy);
+    float s01 = pixel_operator(0.0, dy);
+    float s21 = pixel_operator(0.0, -dy);
+    float s02 = pixel_operator(dx, dy);
+    float s12 = pixel_operator(dx, 0.0);
+    float s22 = pixel_operator(dx, -dy);
+    
+    float sx = s00 + 2.0 * s10 + s20 - (s02 + 2.0 * s12 + s22);
+    float sy = s00 + 2.0 * s01 + s02 - (s20 + 2.0 * s21 + s22);
+    float dist = sx * sx + sy * sy;
+    
+    return dist;
+}
+
+
+
+void main()
+{	
+    //vec3 Kd = vColor.rgb;
+    vec4 texColor = texture2D( baseTexture, texcoord);
+
+    // predefine var
+    float gradientThreshold = 0.05;    
+    float graylevel = sobel_filter();
+    
+    if (graylevel > gradientThreshold)
+    {
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    } 
+    else 
+    {
+      discard;
+    
+        gl_FragColor = texColor;
+    }
+
+  	
+}
