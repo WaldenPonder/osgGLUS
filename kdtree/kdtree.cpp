28e44087b9f92b8e23a2a780e9bd12867e1f90fe
wangstu <wangqianstu@163.com>
2019-11-26 10:02:25 +0800

    -

--- kdtree/kdtree.cpp
+++ kdtree/kdtree.cpp
@@ -35,39 +35,49 @@ struct Node
 	Node* p = nullptr;
 };
 
-void build(POINTS& pts, int plane, Node* parent)
-{
-	if (pts.size() < 2) return;
-
-	POINTS l, r;
-	split(pts, plane, parent, l, r);
-	build(l, plane + 1, parent->l);
-	build(r, plane + 1, parent->r);
-}
-
+//--------------------------------------------------split
 void split(POINTS& origin, int plane, Node* node, POINTS& left, POINTS& right)
 {
 	sort(origin.begin(), origin.end(), [=](Point& p1, Point& p2) { p1.get(plane) < p2.get(plane); });
-	int mid = origin.size() / 2;
+	int mid   = origin.size() / 2;
 	node->val = origin[mid];
 
-	left = POINTS(origin.begin(), origin.end() + mid);
-	right = POINTS(origin.begin() + mid, origin.end());
-	
-	node->l = new Node;
-	node->r = new Node;
+	left	   = POINTS(origin.begin(), origin.end() + mid);
+	node->l	= new Node;
+	node->l->p = node;
+
+	if (mid == 0) return;
+
+	right	  = POINTS(origin.begin() + mid, origin.end());
+	node->r	= new Node;
+	node->r->p = node;
+}
+
+//--------------------------------------------------build
+void build(POINTS& pts, int plane, Node* node)
+{
+	if (pts.size() == 0) return;
+	else if (pts.size() == 1)
+	{
+		node->val = pts[0];
+		return;
+	}
+
+	POINTS l, r;
+	split(pts, plane, node, l, r);
+	build(l, plane + 1, node->l);
+	build(r, plane + 1, node->r);
 }
 
 int main()
 {
-	PTs.push_back(Point(1, 2));
-	PTs.push_back(Point(2, 4));
-	PTs.push_back(Point(1, 5));
-	PTs.push_back(Point(3, 2));
-	PTs.push_back(Point(5, 8));
-	PTs.push_back(Point(10, 8));
-	PTs.push_back(Point(4, 9));
-
+	PTs.push_back(Point(7, 2));
+	PTs.push_back(Point(5, 4));
+	PTs.push_back(Point(2, 3));
+	PTs.push_back(Point(4, 7));
+	PTs.push_back(Point(8, 1));
+	PTs.push_back(Point(9, 6));
+	
 	Node* root = new Node;
 	root->plane = 0;
 
