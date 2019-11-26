0bb23864363198e89801a3e0310e5d04122ec1fd
wangstu <wangqianstu@163.com>
2019-11-26 11:01:12 +0800

    -

--- kdtree/kdtree.cpp
+++ kdtree/kdtree.cpp
@@ -1,8 +1,10 @@
 ﻿#include "pch.h"
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
+
+//https://blog.csdn.net/ye1215172385/article/details/80214776
 
 struct Point
 {
@@ -13,9 +15,19 @@ struct Point
 	{
 	}
 
-	float get(int plane)
+	bool operator==(Point& o)
 	{
-		if (plane % 2 == 0)
+		return o.x == x && o.y == y && o.z == z;
+	}
+
+	bool operator!=(Point& o)
+	{
+		return !operator==(o);
+	}
+
+	float get(int level)
+	{
+		if (level % 2 == 0)
 			return x;
 		else
 			return y;
@@ -30,7 +42,7 @@ struct Node
 	Node() {}
 
 	Point val;
-	int   plane;
+	int	  level;
 
 	Node* l = nullptr;
 	Node* r = nullptr;
@@ -40,19 +52,41 @@ struct Node
 //--------------------------------------------------split
 void split(POINTS& origin, int plane, Node* node, POINTS& left, POINTS& right)
 {
-	sort(origin.begin(), origin.end(), [=](Point& p1, Point& p2) { p1.get(plane) < p2.get(plane); });
-	int mid   = origin.size() / 2;
+	//sort(origin.begin(), origin.end(), [=](auto& p1, auto& p2) { p1.get(plane) < p2.get(plane); });
+
+	for (int i = 0; i < origin.size(); i++)
+	{
+		for (int j = i + 1; j < origin.size(); j++)
+		{
+			if (origin[j].get(plane) < origin[i].get(plane))
+			{
+				std::swap(origin[i], origin[j]);
+			}
+		}
+	}
+
+	int mid	  = origin.size() / 2;
 	node->val = origin[mid];
 
-	left	   = POINTS(origin.begin(), origin.end() + mid);
-	node->l	= new Node;
-	node->l->p = node;
+	if (origin.size() >= 3)
+	{
+		left		   = POINTS(origin.begin(), origin.begin() + mid);
+		node->l		   = new Node;
+		node->l->p	   = node;
+		node->l->level = plane + 1;
 
-	if (mid == 0) return;
-
-	right	  = POINTS(origin.begin() + mid, origin.end());
-	node->r	= new Node;
-	node->r->p = node;
+		right		   = POINTS(origin.begin() + mid + 1, origin.end());
+		node->r		   = new Node;
+		node->r->p	   = node;
+		node->r->level = plane + 1;
+	}
+	else if (origin.size() == 2)
+	{
+		left		   = POINTS(origin.begin(), origin.begin() + mid);
+		node->l		   = new Node;
+		node->l->p	   = node;
+		node->l->level = plane + 1;
+	}
 }
 
 //--------------------------------------------------build
@@ -81,9 +115,8 @@ int main()
 	PTs.push_back(Point(8, 1));
 	PTs.push_back(Point(9, 6));
 
-	Node* root  = new Node;
-	root->plane = 0;
-
+	Node* root = new Node;
+	root->level = 0;
 	build(PTs, 0, root);
 
 	getchar();
